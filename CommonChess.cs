26e374fb98466322f1e0b71a211d10dda14fe975
Luca Bolognese <lucabol@microsoft.com>
2017-10-19 21:56:55 +0200

    Add candidate calculation

--- CommonChess.cs
+++ CommonChess.cs
@@ -5,6 +5,20 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+public class CandidateMove
+{
+    public CandidateMove() {
+        BestContinuation = new List<string>();
+        NextCandidates = new List<CandidateMove>();
+    }
+
+    public int Depth { get; set; }
+    public string Move { get; set; }
+    public List<string> BestContinuation { get; set; }
+    public int Score { get; set; } // In centipeds
+    public List<CandidateMove> NextCandidates { get; set; }
+}
+
 public static class CommonChess
 {
     static readonly int requestTimeout = 10 * 1_000; // 10 secs
@@ -60,6 +74,49 @@ public static class CommonChess
         string errText = process.StandardError.ReadToEnd();
 
         return (outText, errText);
+    }
+
+    public static CandidateMove LineToCandidate(string line)
+    {
+        if (String.IsNullOrEmpty(line)) return null;
+
+        var tokens = line.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if(tokens[0] != "info") return null; // skip over non-info
+
+        var c = new CandidateMove();
+
+        var idx = Array.FindIndex(tokens, x => x == "pv");
+        if (idx == -1) return null; // skip over different info lines
+        c.Move = tokens[idx + 1];
+        for(int i = idx + 2; i < tokens.Length; i++)
+        {
+            c.BestContinuation.Add(tokens[i]);
+        }
+        
+        idx = Array.FindIndex(tokens, x => x == "score");
+        if (idx == -1) return null; // skip over different info lines
+        c.Score = int.Parse(tokens[idx + 2]);
+
+        idx = Array.FindIndex(tokens, x => x == "depth");
+        if (idx == -1) return null; // skip over different info lines
+        c.Depth = int.Parse(tokens[idx + 1]);
+
+        return c;
+    }
+
+    public static IEnumerable<CandidateMove> EngineTextToCandidates(string text)
+    {
+        var lines = text.Split(new char[] { '\n' }, StringSplitOptions.None);
+        return lines.Select(LineToCandidate).Where(x => x != null);
+    }
+
+    public static CandidateMove[] GetCandidateMoves(string engineExe, string workingDir, string fen)
+    {
+        var depth = 3;
+        var commands = $"ucinewgame\nsetoption name MultiPV value 50\nposition fen {fen}\ngo depth {depth}";
+        (var outText, var errText) = GetEngineText(engineExe, workingDir, commands);
+        return EngineTextToCandidates(outText).Where(c => c.Depth == depth).ToArray();
     }
 }
 
--- HumanMoves.cs
+++ HumanMoves.cs
@@ -9,12 +9,22 @@ using Microsoft.Azure.WebJobs.Host;
 public static class HumanMoves
 {
     [FunctionName("HumanMoves")]
-    public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "chess/engine/{engineName}/{fen}")]HttpRequestMessage req, string engineName, string fen, TraceWriter log)
+    public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "chess/engine/{engineName}/HumanMoves")]HttpRequestMessage req, string engineName, TraceWriter log)
     {
-        log.Info("C# HTTP trigger function processed a request.");
+        log.Info($"Asked for engine {engineName}");
+        var engineCommand = CommonChess.GetEngineCommand(engineName);
+        log.Info($"Engine command is {engineCommand}");
 
+        var workingDir = CommonWeb.GetWorkingDirectory(req);
+        log.Info($"Working dir is {workingDir}");
+
+       string fen1 = req.GetQueryNameValuePairs()
+            .FirstOrDefault(q => string.Compare(q.Key, "fen", true) == 0)
+            .Value;
+
+        var candidates = CommonChess.GetCandidateMoves(engineCommand, workingDir, WebUtility.UrlDecode(fen1));
         // Fetching the name from the path parameter in the request URL
-        return req.CreateResponse(HttpStatusCode.OK, "Hello " + fen);
+        return req.CreateResponse(HttpStatusCode.OK, candidates);
     }
 }
 
